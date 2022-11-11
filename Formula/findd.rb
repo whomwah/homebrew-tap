eb9615ec880dab6b9696776d5faa5bc344bd0e71
Duncan Robertson <duncan@whomwah.com>
2022-11-11 12:18:48 +0000

    findd: add 0.0.2 bottle.

--- Formula/findd.rb
+++ Formula/findd.rb
@@ -5,6 +5,12 @@ class Findd < Formula
   sha256 "c7ebab1ab2e352552cffb1955c54b4ba761fe38a1339838dd10e0366a1f82668"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/whomwah/homebrew-tap/releases/download/findd-0.0.2"
+    sha256 cellar: :any_skip_relocation, monterey:     "8cdd35cb40c48b5cf0e2e9be714840c4ace03130c54aeea081cba00887061b6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "262499a65e85cebb966b6fb705001e992e21dd31366dbbd166db581874433153"
+  end
+
   def install
     system "make VERSION='0.0.2'"
     bin.install "findd"
